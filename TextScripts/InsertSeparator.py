9d14a4b6dcc6446f8849585c02a384e674e38bf0
Tom Seddon <tom@tmbp-w7>
2013-05-23 15:11:14 +0100

    Tweak some stuff.

--- TextScripts/InsertSeparator.py
+++ TextScripts/InsertSeparator.py
@@ -1,5 +1,11 @@
 #!env python
 #XcodeMenuName: Insert separator
-import sys
+import sys,os
 
 sys.stdout.write("//////////////////////////////////////////////////////////////////////////\n")
+
+if os.getenv("windir") is not None:
+    # workaround for stupid .NET annoyance... must fix this better.
+    sys.stdout.write("\n")
+
+    
