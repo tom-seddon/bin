5cbf57e2c421bce5e96d90df3c3a1c966a376073
Tom Seddon <tom@tlinux>
2015-04-09 16:47:55 +0100

    Make dump.py pipe-friendly.

--- dump.py
+++ dump.py
@@ -1,6 +1,46 @@
 #!/usr/bin/python
 import optparse,sys,glob
 
+def dump(f):
+    offset=0
+
+    cols=range(options.cols)
+
+    while 1:
+        row=f.read(options.cols)
+
+        if row=="":
+            break
+
+        line="%08X: %s  %s\n"%(offset,
+                               " ".join(["%02X"%ord(row[i]) if i<len(row) else "  " for i in cols]),
+                               "".join([(row[i] if ord(row[i])>=32 and ord(row[i])<=126 else ".") if i<len(row) else " " for i in cols]))
+
+        # line="%08X: "%offset
+
+        # for i in range(options.cols):
+        #     if i<len(row):
+        #         line+=" %02X"%ord(row[i])
+        #     else:
+        #         line+="   "
+
+        # line+="  "
+
+        # for i in range(options.cols):
+        #     if i<len(row):
+        #         if ord(row[i])>=32 and ord(row[i])<=126:
+        #             line+=row[i]
+        #         else:
+        #             line+="."
+        #     else:
+        #         line+=" "
+
+        # line+="\n"
+
+        sys.stdout.write(line)
+
+        offset+=options.cols
+
 def main(options,
          args):
     if len(args)<1:
@@ -15,53 +55,16 @@ def main(options,
         sys.stderr.write("FATAL: given name(s) don't appear to match any file(s).")
         sys.exit(1)
 
-    out=sys.stdout
-
     try:
         for file_name in file_names:
             if len(file_names)>1:
-                out.write("\n** %s:\n\n"%file_name)
+                sys.stdout.write("\n** %s:\n\n"%file_name)
 
-            with open(file_name,
-                      "rb") as f:
-                offset=0
-
-                cols=range(options.cols)
-
-                while 1:
-                    row=f.read(options.cols)
-
-                    if row=="":
-                        break
-
-                    line="%08X: %s  %s\n"%(offset,
-                                           " ".join(["%02X"%ord(row[i]) if i<len(row) else "  " for i in cols]),
-                                           "".join([(row[i] if ord(row[i])>=32 and ord(row[i])<=126 else ".") if i<len(row) else " " for i in cols]))
-
-                    # line="%08X: "%offset
-
-                    # for i in range(options.cols):
-                    #     if i<len(row):
-                    #         line+=" %02X"%ord(row[i])
-                    #     else:
-                    #         line+="   "
-
-                    # line+="  "
-
-                    # for i in range(options.cols):
-                    #     if i<len(row):
-                    #         if ord(row[i])>=32 and ord(row[i])<=126:
-                    #             line+=row[i]
-                    #         else:
-                    #             line+="."
-                    #     else:
-                    #         line+=" "
-
-                    # line+="\n"
-
-                    out.write(line)
-
-                    offset+=options.cols
+            if file_name=="-":
+                dump(sys.stdin)
+            else:
+                with open(file_name,"rb") as f:
+                    dump(f)
     except IOError,e:
         if e.errno==32:
             # Broken pipe. Just ignore this.
