33d33aaa2a39ce4aa341dd411034cf7f3ff38a8b
Tom Seddon <tom@tmbp>
2016-01-15 14:58:12 +0000

    Improve find_dup.
    - reverse sort
    - exclude pattern
    - doesn't barf when number of files in folder exceeds max fd count
      (assumed to be 500) - though examining such folders is markedly
      slower

--- find_dup.py
+++ find_dup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # -*- mode:python -*-
-import argparse,sys,os,os.path,hashlib,stat
+import argparse,sys,os,os.path,hashlib,stat,fnmatch
 
 g_verbose=False
 
 ##########################################################################
 ##########################################################################
 
-def v(str):
+def pv(str):
     global g_verbose
 
     if g_verbose:
@@ -21,14 +21,28 @@ def v(str):
     #     for i in range(len(filekeys)):
     #         files=files_by_filekey[filekeys[i]]
 
-    #         v(prefix if i==0 else " "*len(prefix))
+    #         pv(prefix if i==0 else " "*len(prefix))
 
-    #         v(str(files))
-    #         v("\n")
+    #         pv(str(files))
+    #         pv("\n")
 
 ##########################################################################
 ##########################################################################
-        
+
+g_last_progress_msg=None
+
+def print_progress(index,num_items,value,total):
+    global g_last_progress_msg
+    
+    if sys.stdout.isatty():
+        perc=int(value/float(total)*1000)
+        msg="%d/%d: %03d.%d%%"%(index+1,num_items,perc/10,perc%10)
+
+        if msg!=g_last_progress_msg:
+            g_last_progress_msg=msg
+            sys.stdout.write("\r"+g_last_progress_msg)
+            sys.stdout.flush()
+
 def main(options):
     global g_verbose
 
@@ -41,19 +55,35 @@ def main(options):
     # retrieve everything
     for dir in options.dirs:
         for dir_path,dir_names,file_names in os.walk(dir):
-            v("    Looking in \"%s\"...\n"%dir_path)
+            for i in range(len(dir_names)-1,-1,-1):
+                if dir_names[i] in ["$RECYCLE.BIN",
+                                    ".Trashes",
+                                    ".Trash-1000",
+                                    ".DS_Store",
+                                    ".fseventsd"]:
+                    del dir_names[i]
+                    
+            pv("    Looking in \"%s\"...\n"%dir_path)
             for file_name in file_names:
-                full_name=os.path.join(dir_path,file_name)
+                keep=True
 
-                s=os.lstat(full_name)
+                for exclude in options.excludes:
+                    if fnmatch.fnmatch(file_name,exclude):
+                        keep=False
+                        break
 
-                if stat.S_ISLNK(s.st_mode):
-                    pass
-                else:
-                    filekey=(s.st_dev,s.st_ino)
+                if keep:
+                    full_name=os.path.join(dir_path,file_name)
 
-                    files_by_filekey.setdefault(filekey,[]).append(full_name)
-                    filekeys_by_size.setdefault(s.st_size,[]).append(filekey)
+                    s=os.lstat(full_name)
+
+                    if stat.S_ISLNK(s.st_mode):
+                        pass
+                    else:
+                        filekey=(s.st_dev,s.st_ino)
+
+                        files_by_filekey.setdefault(filekey,[]).append(full_name)
+                        filekeys_by_size.setdefault(s.st_size,[]).append(filekey)
 
     # remove anything uninteresting...
     for size,filekeys in filekeys_by_size.items():
@@ -65,52 +95,73 @@ def main(options):
 
     #sys.exit(1)
 
-    sorted_sizes=sorted(filekeys_by_size.keys())
-    for size_idx in range(len(sorted_sizes)):
-        size=sorted_sizes[size_idx]
+    sorted_sizes=sorted(filekeys_by_size.keys(),reverse=options.reverse_sort)
+    
+    for size_idx,size in enumerate(sorted_sizes):
         filekeys=filekeys_by_size[size]
-        
-        fs=[]
-        digests=[]
-        for filekey in filekeys:
-            fs.append(open(files_by_filekey[filekey][0],"rb"))
-            digests.append(hashlib.md5())
-
-        last_msg=""
-        chunk_size=8192
-        for offset in range(0,size,chunk_size):
-            remaps={}
-            for i in range(len(fs)):
-                if fs[i] is not None:
-                    chunk=fs[i].read(chunk_size)
-                    digests[i].update(chunk)
-                    remaps.setdefault(chunk,[]).append(i)
-
-            if sys.stdout.isatty():
-                perc=int(offset/float(size)*1000)
-                msg="%d/%d; %03d.%d%%"%(size_idx+1,len(sorted_sizes),perc/10,perc%10)
-
-                if msg!=last_msg:
-                    last_msg=msg
-                    sys.stdout.write("\r"+last_msg)
-                    sys.stdout.flush()
-
-            if len(remaps)==0:
-                # no dups...
-                break
-
-            for chunk,indexes in remaps.items():
-                if len(indexes)==1:
-                    fs[indexes[0]].close()
-                    fs[indexes[0]]=None
-
-        sys.stdout.write("\r"+len(last_msg)*" "+"\r")
 
         groups={}
-        for i in range(len(fs)):
-            if fs[i] is not None:
-                groups.setdefault(digests[i].hexdigest(),[]).append(filekeys[i])
+        
+        if len(filekeys)<500:
+            fs=[]
+            digests=[]
+            for filekey in filekeys:
+                fs.append(open(files_by_filekey[filekey][0],"rb"))
+                digests.append(hashlib.md5())
 
+            last_msg=""
+            chunk_size=8192
+            for offset in range(0,size,chunk_size):
+                remaps={}
+                for i in range(len(fs)):
+                    if fs[i] is not None:
+                        chunk=fs[i].read(chunk_size)
+                        digests[i].update(chunk)
+                        remaps.setdefault(chunk,[]).append(i)
+
+                print_progress(size_idx,len(sorted_sizes),offset,size)
+
+                if len(remaps)==0:
+                    # no dups...
+                    break
+
+                for chunk,indexes in remaps.items():
+                    if len(indexes)==1:
+                        fs[indexes[0]].close()
+                        fs[indexes[0]]=None
+
+            sys.stdout.write("\r"+len(last_msg)*" "+"\r")
+
+            for i in range(len(fs)):
+                if fs[i] is not None:
+                    groups.setdefault(digests[i].hexdigest(),[]).append(filekeys[i])
+
+            for f in fs:
+                if f is not None:
+                    f.close()
+
+            del fs
+        else:
+            nbytes_total=len(filekeys)*size
+            nbytes_seen=0
+            chunk_size=1048576
+
+            for filekey in filekeys:
+                with open(files_by_filekey[filekey][0],"rb") as f:
+                    digest=hashlib.md5()
+
+                    for offset in range(0,size,chunk_size):
+                        chunk=f.read(chunk_size)
+                        digest.update(chunk)
+                        nbytes_seen+=len(chunk)
+
+                        print_progress(size_idx,len(sorted_sizes),nbytes_seen,nbytes_total)
+
+                    groups.setdefault(digest.hexdigest(),[]).append(filekey)
+
+            for key,val in groups.items():
+                if len(val)==1: del groups[key]
+        
         if len(groups)>0:
             for hexdigest,filekeys in groups.items():
                 lines=[hexdigest+": ",
@@ -119,17 +170,12 @@ def main(options):
                 width=max([len(line) for line in lines])
 
                 lines=[(width*" "+line)[-width:] for line in lines]
-                
+
                 for i in range(len(filekeys)):
                     sys.stdout.write(lines[i] if i<len(lines) else width*" ")
                     sys.stdout.write(files_by_filekey[filekeys[i]][0])
                     sys.stdout.write("\n")
-            
-        for f in fs:
-            if f is not None:
-                f.close()
 
-        del fs
 
 ##########################################################################
 ##########################################################################
@@ -145,6 +191,19 @@ if __name__=="__main__":
                         dest="min_size",
                         nargs=1,
                         help="""Ignore sets totalling less than %(metavar)s bytes. (Default %(metavar)s: %(default)d.)""")
+
+    parser.add_argument("-r",
+                        dest="reverse_sort",
+                        default=False,
+                        action="store_true",
+                        help="""reverse sort order and look at largest files first""")
+
+    parser.add_argument("-x",
+                        dest="excludes",
+                        action="append",
+                        default=[],
+                        metavar="PATTERN",
+                        help="""ignore file(s) with name part matching %(metavar)s""")
 
     parser.add_argument("-v",
                         "--verbose",
