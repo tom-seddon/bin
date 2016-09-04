2e65c375b8197acbfd9a62082e20680985d04eaa
Tom Seddon <tom@tmbp>
2016-09-04 15:11:22 +0100

    Add -b option to dump. Switch to argparse.

--- dump.py
+++ dump.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
-import optparse,sys,glob
+import argparse,sys,glob
 
-def dump(f):
+##########################################################################
+##########################################################################
+
+def dump(f,options):
     offset=0
 
     cols=range(options.cols)
@@ -12,59 +15,30 @@ def dump(f):
         if row=="":
             break
 
-        line="%08X: %s  %s\n"%(offset,
+        line="%08X: %s  %s\n"%(offset+options.base,
                                " ".join(["%02X"%ord(row[i]) if i<len(row) else "  " for i in cols]),
                                "".join([(row[i] if ord(row[i])>=32 and ord(row[i])<=126 else ".") if i<len(row) else " " for i in cols]))
-
-        # line="%08X: "%offset
-
-        # for i in range(options.cols):
-        #     if i<len(row):
-        #         line+=" %02X"%ord(row[i])
-        #     else:
-        #         line+="   "
-
-        # line+="  "
-
-        # for i in range(options.cols):
-        #     if i<len(row):
-        #         if ord(row[i])>=32 and ord(row[i])<=126:
-        #             line+=row[i]
-        #         else:
-        #             line+="."
-        #     else:
-        #         line+=" "
-
-        # line+="\n"
-
+        
         sys.stdout.write(line)
 
         offset+=options.cols
 
-def main(options,
-         args):
-    if len(args)<1:
-        sys.stderr.write("FATAL: must supply name(s) of file to dump.")
-        sys.exit(1)
+##########################################################################
+##########################################################################
 
-    file_names=[]
-    for arg in args:
-        file_names+=glob.glob(arg)
+def main(options):
+    # file_names=[]
+    # for arg in args:
+    #     file_names+=glob.glob(arg)
 
-    if len(file_names)==0:
-        sys.stderr.write("FATAL: given name(s) don't appear to match any file(s).")
-        sys.exit(1)
+    # if len(file_names)==0:
+    #     sys.stderr.write("FATAL: given name(s) don't appear to match any file(s).")
+    #     sys.exit(1)
 
     try:
-        for file_name in file_names:
-            if len(file_names)>1:
-                sys.stdout.write("\n** %s:\n\n"%file_name)
-
-            if file_name=="-":
-                dump(sys.stdin)
-            else:
-                with open(file_name,"rb") as f:
-                    dump(f)
+        if options.file=="-": dump(sys.stdin,options)
+        else:
+            with open(options.file,"rb") as f: dump(f,options)
     except IOError,e:
         if e.errno==32:
             # Broken pipe. Just ignore this.
@@ -72,18 +46,17 @@ def main(options,
         else:
             raise
 
+##########################################################################
+##########################################################################
+
+# http://stackoverflow.com/questions/25513043/python-argparse-fails-to-parse-hex-formatting-to-int-type
+def auto_int(x): return int(x,0)
+
 if __name__=="__main__":
-    parser=optparse.OptionParser(usage="%prog [options] FILE(s)")
+    parser=argparse.ArgumentParser()
 
-    parser.add_option("-c",
-                      "--cols",
-                      dest="cols",
-                      metavar="COLS",
-                      default=16,
-                      type="int",
-                      help="set number of columns to COLS (default %default)")
+    parser.add_argument("-c","--cols",dest="cols",metavar="COLS",default=16,help="set number of columns to COLS")
+    parser.add_argument("-b","--base",default=0,type=auto_int,help="base address for offsets")
+    parser.add_argument("file",metavar="FILE",help="file to dump")
 
-    options,args=parser.parse_args()
-
-    main(options,
-         args)
+    main(parser.parse_args(sys.argv[1:]))
