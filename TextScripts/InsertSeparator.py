8dfd1b7a101aa62bd8a442758255eca4e3a0498a
Tom Seddon <github@tomseddon.plus.com>
2017-07-27 21:54:13 +0100

    Add VSScripts-compatible C# mode.
    Inserting "///" makes Visual Studio insert a C# doc comment, which
    is rarely desirable.

--- TextScripts/InsertSeparator.py
+++ TextScripts/InsertSeparator.py
@@ -2,7 +2,10 @@
 #XcodeMenuName: Insert separator
 import sys,os
 
-sys.stdout.write("//////////////////////////////////////////////////////////////////////////\n")
+if os.getenv("Extension").lower()==".cs":
+    sys.stdout.write("//########################################################################\n")
+else:
+    sys.stdout.write("//////////////////////////////////////////////////////////////////////////\n")
 
 if os.getenv("windir") is not None:
     # workaround for stupid .NET annoyance... must fix this better.
