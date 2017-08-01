3ca42b859f5240289cffe15ad33f16728708e6d2
Tom Seddon <github@tomseddon.plus.com>
2017-08-01 22:25:36 +0100

    Fix dump.py merge mess.

--- dump.py
+++ dump.py
@@ -36,16 +36,13 @@ def main(options):
     #     sys.exit(1)
 
     try:
-        for file_name in file_names:
-            if len(file_names)>1:
+        for file_name in options.file_names:
+            if len(options.file_names)>1:
                 sys.stdout.write("\n** %s:\n\n"%file_name)
 
-            if file_name=="-":
-                dump(sys.stdin)
+            if file_name=="-": dump(sys.stdin,options)
             else:
-                with open(file_name,"rb") as f:
-                    dump(f)
-
+                with open(file_name,"rb") as f: dump(f,options)
     except IOError,e:
         if e.errno==32 or e.errno==22:
             # Broken pipe. Just ignore this.
@@ -76,8 +73,8 @@ def auto_int(x): return int(x,0)
 if __name__=="__main__":
     parser=argparse.ArgumentParser()
 
-    parser.add_argument("-c","--cols",dest="cols",metavar="COLS",default=16,help="set number of columns to COLS")
+    parser.add_argument("-c","--cols",dest="cols",metavar="COLS",default=16,type=auto_int,help="set number of columns to COLS")
     parser.add_argument("-b","--base",default=0,type=auto_int,help="base address for offsets")
-    parser.add_argument("file",metavar="FILE",help="file to dump")
+    parser.add_argument("file_names",metavar="FILE",help="file to dump",action="append")
 
     main(parser.parse_args(sys.argv[1:]))
