6372deee74b47c361ac38c20d61f8c99707d3765
Tom Seddon <tom@tmbp>
2019-07-21 14:00:04 +0100

    Add -x option to pmacs.

--- pmacs.py
+++ pmacs.py
@@ -1,5 +1,5 @@
-#!env python
-import os,os.path,sys,tempfile,subprocess
+#!/usr/bin/python
+import os,os.path,sys,tempfile,subprocess,argparse
 
 ##########################################################################
 ##########################################################################
@@ -13,7 +13,7 @@ except:
 ##########################################################################
 ##########################################################################
 
-def main():
+def main(options):
     data=sys.stdin.read()
 
     temp_fname=os.path.join(tempfile.gettempdir(),"pmacs.%d.dat"%os.getpid())
@@ -30,28 +30,33 @@ def main():
             fname_chars+="%c"
             fname_bytes.append(str(ord(c)))
 
-    elisp=[
-        r'(let ((bufname (let ((i 0)',
-        r'                     bufname)',
-        r'                 (while (progn',
-        r'                          (setq bufname (concat "*pmacs"',
-        r'                                                (if (= i 0)',
-        r'                                                    ""',
-        r'                                                  (int-to-string i))',
-        r'                                                "*"))',
-        r'                          (setq i (1+ i))',
-        r'                          (get-buffer bufname)))',
-        r'                 bufname)))',
-        r'  (let ((fname (format "%s" %s)))'%(fname_chars," ".join(fname_bytes)),
-        r'    (find-file fname)',
-        r'    (fundamental-mode)',
-        r'    (set-visited-file-name nil)',
-        r'    (rename-buffer bufname)',
-        r'    (delete-file fname)',
-        r'    (message "got %d bytes" (- (point-max) (point-min)))))',
-    ]
+    elisp=[]
+    elisp+=[r'(let ((bufname (let ((i 0)']
+    elisp+=[r'                     bufname)']
+    elisp+=[r'                 (while (progn']
+    elisp+=[r'                          (setq bufname (concat "*pmacs"']
+    elisp+=[r'                                                (if (= i 0)']
+    elisp+=[r'                                                    ""']
+    elisp+=[r'                                                  (int-to-string i))']
+    elisp+=[r'                                                "*"))']
+    elisp+=[r'                          (setq i (1+ i))']
+    elisp+=[r'                          (get-buffer bufname)))']
+    elisp+=[r'                 bufname)))']
+    elisp+=[r'  (let ((fname (format "%s" %s)))'%(fname_chars," ".join(fname_bytes))]
+    elisp+=[r'    (find-file fname)']
+    elisp+=[r'    (fundamental-mode)']
+    elisp+=[r'    (set-visited-file-name nil)']
+    elisp+=[r'    (rename-buffer bufname)']
+    elisp+=[r'    (delete-file fname)']
+    elisp+=[r'    (message "got %d bytes" (- (point-max) (point-min)))']
 
-    #print elisp
+    for command in options.commands:
+        elisp+=['    (command-execute \'%s)'%command]
+
+    elisp+=[r'  ))']
+
+    # if options.verbose:
+    #     print>>sys.stderr,elisp
 
     # hmm...
     if sys.platform=="darwin":
@@ -75,9 +80,20 @@ def main():
 ##########################################################################
 ##########################################################################
 
+def pmacs(argv):
+    parser=argparse.ArgumentParser()
+
+    parser.add_argument('-v','--verbose',action='store_true',help='be more verbose')
+    parser.add_argument('-x',action='append',default=[],dest='commands',metavar='COMMAND',help='do (in effect) M-x %(metavar)s once file is loaded')
+
+    main(parser.parse_args(argv))
+
+##########################################################################
+##########################################################################
+
 if __name__=="__main__":
     if got_msvcrt:
         msvcrt.setmode(sys.stdin.fileno(),os.O_BINARY)
-        
-    main()
+
+    pmacs(sys.argv[1:])
 
